b364fffbb8a4b34c547fe8b96d9fb578d22cda7f
samane ghodrati <samane.ghodrati@gmail.com>
2025-02-09 00:15:18 -0900

    add

--- java.js
+++ java.js
@@ -1,1 +1,9 @@
 const url="http://api.weatherstack.com/current?access_key=1a8ebfbf572cb18c9c9046de80da2e29&query=Tehran";
+async function weather() {
+    const dat= await fetch(url);
+
+    const data=await dat.json();
+    console.log(data.current.pressure);
+    
+}
+weather();
